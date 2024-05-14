fd71d5c6acce8d84844a4472f884ebea77870a96
Данил Чирков <chirkovdanil04@mail.ru>
2024-05-15 02:03:47 +0300

    finalapi

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # api_final
-api final
+
+### API для Yatube:
+API для Yatube-проект для блоггеров Yatube.
+Тут не только люди, но и блоггеры могут создавать посты, писать комментарии, подписываться друг на друга и многое другое.
+
+### Как запустить проект:
+
+Клонировать репозиторий и перейти в него в командной строке, например так:
+```
+git clone <ссылка с GitHub>
+```
+
+```
+cd api_final_yatube
+```
+Cоздать и активировать виртуальное окружение:
+
+```
+python -m venv venv
+```
+
+```
+source venv/Scripts/activate
+```
+
+```
+python3 -m pip install --upgrade pip
+```
+
+Установить зависимости из файла requirements.txt:
+
+```
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+
+```
+python manage.py migrate
+```
+
+Запустить проект:
+
+```
+python manage.py runserver
+```
+### Некоторые примеры запросов к API:
+Получаем список всех постов или создаём новый пост(GET,POST):
+```
+api/v1/posts/
+```
+Получаем, редактируем или удаляем пост с идентификатором{post_id}(GET, PUT, PATCH, DELETE):
+```
+api/v1/posts/{post_id}/
+```
+Получаем список всех комментариев поста с идентификатором post_id(GET):
+```
+api/v1/posts/{post_id}/comments/
+```
+Cоздаём новый комментарий для поста с идентификатором {post_id}(POST):
+```
+api/v1/posts/{post_id}/comments/
+```
+Получаем, редактируем или удаляем комментарий с идентификатором {comment_id} в посте с id=post_id(GET, PUT, PATCH, DELETE):
+```
+api/v1/posts/{post_id}/comments/{comment_id}/
+```
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,54 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets, permissions
+from rest_framework.pagination import LimitOffsetPagination
+from django.shortcuts import get_object_or_404
+from rest_framework import filters
+
+from posts.models import Post, Group, Follow
+from .serializers import PostSerializer, CommentSerializer, GroupSerializer
+from .serializers import FollowSerializer
+from .permissions import AuthorOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AuthorOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+    def get_post(self):
+        post_id = self.kwargs.get('post_id')
+        return get_object_or_404(Post, pk=post_id)
+
+    def get_queryset(self):
+        new_queryset = self.get_post().comments.all()
+        return new_queryset
+
+    def perform_create(self, serializer):
+        author = self.request.user
+        serializer.save(author=author, post=self.get_post())
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
